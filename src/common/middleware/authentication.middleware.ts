12301fee5be65eedb7ca945415a93ab26d8d2036
denam98 <dchamath5@gmail.com>
2024-04-05 14:31:04 +0530

    unnecessary logs removed

--- src/common/middleware/authentication.middleware.ts
+++ src/common/middleware/authentication.middleware.ts
@@ -20,11 +20,6 @@ export class AuthenticationMiddleware implements NestMiddleware {
 
       if (decoded && decoded.sub && decoded.sub.id) {
         this.requestService.setUserId(decoded.sub.id);
-        this.errorService.printLog(
-          'info',
-          AuthenticationMiddleware.name,
-          'userId: ' + this.requestService.getUserId(),
-        );
       }
 
       next();
